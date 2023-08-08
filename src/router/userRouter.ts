da27fab58079ed549788e5ad18b7a6262cfe5adc
Julia-slvbrg <julia.slvb@gmail.com>
2023-08-08 19:45:44 -0300

    feat: CRUD users implemented

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -6,6 +6,6 @@ export const userRouter = express.Router();
 const userController = new UserController();
 
 userRouter.get('/', userController.getUsers);
-userRouter.post('/', userController.createUser);
+userRouter.post('/signup', userController.createUser);
 userRouter.put('/:id', userController.updateUser);
 userRouter.delete('/:id', userController.deleteUserById)
--- src/types.ts
+++ src/types.ts
@@ -21,10 +21,16 @@ export interface LikesDislikesDB{
     user_id: string,
     post_id: string,
     like: number
-}
+};
 
 export enum USER_ROLES{
     NORMAL = 'NORMAL',
     ADMIN = 'ADMIN'
+};
+
+export interface TokenPayload{
+    id: string,
+    name: string,
+    role: USER_ROLES
 }
 
